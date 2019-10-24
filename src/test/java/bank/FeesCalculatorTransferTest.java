ef35ad232939810209ce74f129e8c01f7d5991e7
Jordan Sne <jordansne@gmail.com>
2019-10-23 21:05:35 -0400

    Move transfer tests to its own class

--- src/test/java/bank/FeesCalculatorTransferTest.java
+++ src/test/java/bank/FeesCalculatorTransferTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(JUnit4.class)
-public class FeesCalculatorTest {
+public class FeesCalculatorTransferTest {
 
     private FeesCalculator calculator;
 
@@ -20,97 +20,97 @@ public class FeesCalculatorTest {
     }
 
     @Test
-    public void calculatesTransferWhenStudentHighTransHighFromHighTo() {
+    public void transferWhenStudentHighTransHighFromHighTo() {
         int expected = (int) round(120000 * percent(0.125));
         assertThat(calculator.calculateTransferFee(120000, 150000, 150000, true), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenStudentHighTransHighFromLowTo() {
+    public void transferWhenStudentHighTransHighFromLowTo() {
         int expected = (int) round(120000 * percent(0.25));
         assertThat(calculator.calculateTransferFee(120000, 150000, 60000, true), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenStudentHighTransLowFromLowTo() {
+    public void transferWhenStudentHighTransLowFromLowTo() {
         int expected = (int) round(120000 * percent(0.05));
         assertThat(calculator.calculateTransferFee(120000, 60000, 60000, true), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenStudentLowTransLowFromLowTo() {
+    public void transferWhenStudentLowTransLowFromLowTo() {
         int expected = (int) round(50000 * percent(0.1));
         assertThat(calculator.calculateTransferFee(50000, 60000, 60000, true), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenStudentLowTransHighFromHighTo() {
+    public void transferWhenStudentLowTransHighFromHighTo() {
         int expected = (int) round(50000 * percent(0.25));
         assertThat(calculator.calculateTransferFee(50000, 150000, 150000, true), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenStudentHighTransLowFromHighTo() {
+    public void transferWhenStudentHighTransLowFromHighTo() {
         int expected = (int) round(120000 * percent(0.025));
         assertThat(calculator.calculateTransferFee(120000, 60000, 150000, true), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenStudentLowTransLowFromHighTo() {
+    public void transferWhenStudentLowTransLowFromHighTo() {
         int expected = (int) round(50000 * percent(0.05));
         assertThat(calculator.calculateTransferFee(50000, 60000, 150000, true), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenStudentLowTransHighFromLowTo() {
+    public void transferWhenStudentLowTransHighFromLowTo() {
         int expected = (int) round(50000 * percent(0.25));
         assertThat(calculator.calculateTransferFee(50000, 150000, 60000, true), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenNonStudentHighTransHighFromHighTo() {
+    public void transferWhenNonStudentHighTransHighFromHighTo() {
         int expected = (int) round(120000 * percent(0.25));
         assertThat(calculator.calculateTransferFee(120000, 150000, 150000, false), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenNonStudentHighTransHighFromLowTo() {
+    public void transferWhenNonStudentHighTransHighFromLowTo() {
         int expected = (int) round(120000 * percent(0.5));
         assertThat(calculator.calculateTransferFee(120000, 150000, 60000, false), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenNonStudentHighTransLowFromLowTo() {
+    public void transferWhenNonStudentHighTransLowFromLowTo() {
         int expected = (int) round(120000 * percent(0.1));
         assertThat(calculator.calculateTransferFee(120000, 60000, 60000, false), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenNonStudentLowTransLowFromLowTo() {
+    public void transferWhenNonStudentLowTransLowFromLowTo() {
         int expected = (int) round(50000 * percent(0.2));
         assertThat(calculator.calculateTransferFee(50000, 60000, 60000, false), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenNonStudentLowTransHighFromHighTo() {
+    public void transferWhenNonStudentLowTransHighFromHighTo() {
         int expected = (int) round(50000 * percent(0.5));
         assertThat(calculator.calculateTransferFee(50000, 150000, 150000, false), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenNonStudentHighTransLowFromHighTo() {
+    public void transferWhenNonStudentHighTransLowFromHighTo() {
         int expected = (int) round(120000 * percent(0.05));
         assertThat(calculator.calculateTransferFee(120000, 60000, 150000, false), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenNonStudentLowTransLowFromHighTo() {
+    public void transferWhenNonStudentLowTransLowFromHighTo() {
         int expected = (int) round(50000 * percent(0.1));
         assertThat(calculator.calculateTransferFee(50000, 60000, 150000, false), is(expected));
     }
 
     @Test
-    public void calculatesTransferWhenNonStudentLowTransHighFromLowTo() {
+    public void transferWhenNonStudentLowTransHighFromLowTo() {
         int expected = (int) round(50000 * percent(1));
         assertThat(calculator.calculateTransferFee(50000, 150000, 60000, false), is(expected));
     }
