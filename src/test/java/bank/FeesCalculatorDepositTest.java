990515bc1ae89926677fac8c7435fc64492b8602
Andrew Katoch <akatoch2@uwo.ca>
2019-10-24 01:31:49 -0400

    Updated Weak Robust Equivalence Classes

--- src/test/java/bank/FeesCalculatorDepositTest.java
+++ src/test/java/bank/FeesCalculatorDepositTest.java
@@ -14,109 +14,61 @@ import banking.exceptions.*;
 @RunWith(JUnit4.class)
 public class FeesCalculatorDepositTest {
 
-//	public int calculateDepositInterest(int amount, int accountBalance, boolean student) {
-	
     private FeesCalculator calculator;
 
     @Before
     public void beforeEach() {
         calculator = new FeesCalculator();
     }
-
-    @Test
-    public void depositWhenStudentHighDepositHighBalance() {
+    
+    @Test //DT1
+    public void depositWhenStudentInvalidInput() {
     	boolean student = true;
-    	int deposit = 200;
-    	int balance = 1500;
-    	double percent = 1;
-        int expected = (int) round(deposit * percent(percent));
-        assertThat(calculator.calculateDepositInterest(deposit, balance, student), is(expected));
-    }
-
-    @Test
-    public void depositWhenStudentHighDepositLowBalance() {
-    	boolean student = true;
-    	int deposit = 200;
+    	int deposit = -50;
     	int balance = 500;
-    	double percent = 0.5;
+    	double percent = 0;
         int expected = (int) round(deposit * percent(percent));
-        assertThat(calculator.calculateDepositInterest(deposit, balance, student), is(expected));
-    }
-
-    @Test
-    public void depositWhenStudentLowDepositHighBalance() {
-    	boolean student = true;
-    	int deposit = 50;
-    	int balance = 10000;
-    	double percent = 0.5;
-        int expected = (int) round(deposit * percent(percent));
-        assertThat(calculator.calculateDepositInterest(deposit, balance, student), is(expected));
+        assertThat(calculator.calculateDepositInterest(deposit * 100, balance * 100, student) / 100, is(expected));
     }
 
     @Test
     public void depositWhenStudentLowDepositLowBalance() {
     	boolean student = true;
     	int deposit = 50;
-    	int balance = 1000;
+    	int balance = 500;
     	double percent = 0;
         int expected = (int) round(deposit * percent(percent));
-        assertThat(calculator.calculateDepositInterest(deposit, balance, student), is(expected));
+        assertThat(calculator.calculateDepositInterest(deposit * 100, balance * 100, student) / 100, is(expected));
     }
 
-    @Test(expected = InvalidAmountException.class)
-    public void depositWhenStudentInvalidDeposit() {
-    	boolean student = true;
-    	int deposit = 50;
-    	int balance = 1000;
-        calculator.calculateDepositInterest(deposit, balance, student);
+    @Test
+    public void depositWhenMpmStudentLowDepositLowBalance() {
+    	boolean student = false;
+    	int deposit = 250;
+    	int balance = 1500;
+    	double percent = 0;
+        int expected = (int) round(deposit * percent(percent));
+        assertThat(calculator.calculateDepositInterest(deposit * 100, balance * 100, student) / 100, is(expected));
+    }
+
+    @Test
+    public void depositWhenNonStudentLowDepositLowBalance() {
+    	boolean student = false;
+    	int deposit = 500;
+    	int balance = 7500;
+    	double percent = 0;
+        int expected = (int) round(deposit * percent(percent));
+        assertThat(calculator.calculateDepositInterest(deposit * 100, balance * 100, student) / 100, is(expected));
     }
 
     @Test
     public void depositWhenNonStudentHighDepositHighBalance() {
     	boolean student = false;
     	int deposit = 1000;
-    	int balance = 10000;
+    	int balance = 20000;
     	double percent = 1;
         int expected = (int) round(deposit * percent(percent));
-        assertThat(calculator.calculateDepositInterest(deposit, balance, student), is(expected));
-    }
-
-    @Test
-    public void depositWhenNonStudentHighDepositLowBalance() {
-    	boolean student = false;
-    	int deposit = 1000;
-    	int balance = 2500;
-    	double percent = 0.5;
-        int expected = (int) round(deposit * percent(percent));
-        assertThat(calculator.calculateDepositInterest(deposit, balance, student), is(expected));
-    }
-
-    @Test
-    public void depositWhenNonStudentLowDepositHighBalance() {
-    	boolean student = false;
-    	int deposit = 250;
-    	int balance = 20000;
-    	double percent = 0.5;
-        int expected = (int) round(deposit * percent(percent));
-        assertThat(calculator.calculateDepositInterest(deposit, balance, student), is(expected));
-    }
-
-    @Test
-    public void depositWhenNonStudentLowDepositLowBalance() {
-    	boolean student = false;
-    	int deposit = 250;
-    	int balance = 5000;
-    	double percent = 0;
-        int expected = (int) round(deposit * percent(percent));
-        assertThat(calculator.calculateDepositInterest(deposit, balance, student), is(expected));
-    }
-
-    @Test(expected = InvalidAmountException.class)
-    public void depositWhenNonStudentInvalidDeposit() {
-    	boolean student = false;
-    	int deposit = 50;
-    	int balance = 1000;
-        calculator.calculateDepositInterest(deposit, balance, student);
+        assertThat(calculator.calculateDepositInterest(deposit * 100, balance * 100, student) / 100, is(expected));
     }
 
     private static double percent(double percentage) {
