a534a8176ae456b8199a9ce750dd323510d9d049
Spencer Ridley <sridley4@uwo.ca>
2019-10-25 22:50:18 -0400

    populate database script added

--- src/test/java/atm/SessionWorstCasePIN.java
+++ src/test/java/atm/SessionWorstCasePIN.java
@@ -1,18 +1,39 @@
-package bank;
+package atm;
 
 import atm.ATM;
 import atm.Session;
+import bank.AccountEntry;
+import bank.BankDatabase;
 import banking.Money;
 import banking.exceptions.InvalidAmountException;
 import banking.exceptions.InvalidPINException;
 import banking.exceptions.InvalidTransactionChoiceException;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class SessionWorstCasePIN {
+
+    @BeforeClass
+    public static void populateDatabase() throws BankDatabase.AccountNotFound {
+        BankDatabase bank = BankDatabase.getInstance();
+        bank.updateAccountInfo(1, new AccountEntry(0, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(1, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(2, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(3, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(4, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(5, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(6, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(7, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(8, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(9, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(10, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(11, "saving", 500000, 500000, 100000, 0));
+        bank.updateAccountInfo(1, new AccountEntry(12, "saving", 500000, 500000, 100000, 0));
+    }
 
     @Test(expected = InvalidPINException.class)
     public void wrong3PINTest() throws InvalidTransactionChoiceException, InvalidAmountException, InvalidPINException {
