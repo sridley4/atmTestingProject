8e222bacee661476fa4e809837b3b955d619dd3a
Jordan Sne <jordansne@gmail.com>
2019-11-29 22:54:06 -0500

    Update test case names for deposit

--- src/test/java/bank/FeeCalculatorDepositTest.java
+++ src/test/java/bank/FeeCalculatorDepositTest.java
@@ -36,14 +36,14 @@ public class FeeCalculatorDepositTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         Object[][] data = new Object[][] {
-                { "PathP1", true, 11000, 110000, 110 },
-                { "PathP2", true, 11000, 90000, 55 },
-                { "PathP3", true, 9000, 600000, 45 },
-                { "PathP4", true, 9000, 400000, 0 },
-                { "PathP5", false, 60000, 600000, 600 },
-                { "PathP6", false, 60000, 400000, 300 },
-                { "PathP7", false, 40000, 1100000, 200 },
-                { "PathP8", false, 40000, 900000, 0 },
+                { "DT1", true, 11000, 110000, 110 },
+                { "DT2", true, 11000, 90000, 55 },
+                { "DT3", true, 9000, 600000, 45 },
+                { "DT4", true, 9000, 400000, 0 },
+                { "DT5", false, 60000, 600000, 600 },
+                { "DT6", false, 60000, 400000, 300 },
+                { "DT7", false, 40000, 1100000, 200 },
+                { "DT8", false, 40000, 900000, 0 },
         };
         return Arrays.asList(data);
     }
